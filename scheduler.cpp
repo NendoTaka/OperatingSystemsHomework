11defcafe0fb5716875e63dd6db2e17dcf8c3fb9
Klayton Hawkins <kh@grok.MissouriState.edu>
2015-12-01 21:25:48 -0600

    Updated the documentation at top

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,1 +1,13 @@
-filler
+/*Course: CSC460, Homework 5
+Programmers: Klayton Hawkins, Kendall Lewis
+Grok Usernames: kh, kl
+Contribution Percentage: 50%, 50%
+Completed Subtasks:
+Klayton: Piping data to children, choosing process, and commenting
+Kendall: Child file, Makefile, and data output
+Last Modified Date: December 1, 2015
+Description:This program creates children to estimate integral
+Files: makefile Scheduler.cpp child.cpp
+Instructions for compilation and execution:
+1. type: make
+2. type: ./scheduler n (to execute)*/
